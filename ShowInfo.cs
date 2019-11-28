f12fbc6e393a90bc58ade73d8d6df705ccdfb863
Amartya Dutta <56846056+BakaZoro@users.noreply.github.com>
2019-11-28 11:03:49 +0530

    Add files via upload

---
+++ ShowInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ShowInfo : MonoBehaviour {
+
+	public GameObject Info;
+	private bool Show=false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ShowAndHide(){
+
+		if (!Show) {
+		
+			Info.SetActive (true);
+			float i;
+			for (i = 0; i <= 4f; i += Time.deltaTime)
+				;
+			Show = true;
+		} else {
+			Info.SetActive (false);
+			Show = false;
+		}
+	}
+
+}
